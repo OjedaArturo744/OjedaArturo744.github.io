a5f689269ab13948c20bbfc6bb18aa1851a18d60
OjedaArturo744 <ojedaarturo744@gmail.com>
2025-04-25 18:03:43 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,19 +59,15 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Arturo";
             $("#favicon").attr("href", "assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
         }
     });
 
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["desarrollo frontend", "desarrollo backend", "diseño web", "desarrollo Android", "desarrollo web"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
